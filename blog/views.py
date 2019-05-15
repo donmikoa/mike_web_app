3ae7b7e2e183506bb2814a6e9f5a16f76127e77c
michaeliyaomolere <mikoa04@gmail.com>
2019-05-15 16:10:54 -0400

    Creating the about.html page

--- blog/views.py
+++ blog/views.py
@@ -4,18 +4,19 @@ from django.shortcuts import render
 # Creating the views
 post = [
     {
-        'author': 'CoreyMS',
+        'author': 'Michael',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'August 27, 2018'
     },
     {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
+        'author': 'Deji Iyaomolere',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
     }
 ]
+
 
 def home(request):
     return render(request, 'blog/home.html')
