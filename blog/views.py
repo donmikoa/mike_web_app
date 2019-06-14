2bd093828b1b2eea8d7faf9a6c4441dbb0c9eccc
michaeliyaomolere <mikoa04@gmail.com>
2019-06-14 09:58:14 -0400

    Updating forms.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
@@ -25,6 +26,9 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
 
 
 def about(request):
