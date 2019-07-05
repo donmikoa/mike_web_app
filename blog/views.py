3387870f5c559948f8a33bc0a059fdab6aaad42f
michaeliyaomolere <mikoa04@gmail.com>
2019-07-05 18:42:16 -0400

    Creating the Detailed View

--- blog/views.py
+++ blog/views.py
@@ -16,11 +16,9 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
-class PostDetailView(ListView):
+
+class PostDetailView(DetailView):
     model = Post
-    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
-    context_object_name = 'posts'
-    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
