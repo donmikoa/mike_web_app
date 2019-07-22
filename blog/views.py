6f16bfac69995c0492784ad49595beeeab597504
michaeliyaomolere <mikoa04@gmail.com>
2019-07-22 17:33:46 -0400

    Creating a post-form

--- blog/views.py
+++ blog/views.py
@@ -31,6 +31,7 @@ class PostCreateView(CreateView):
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
