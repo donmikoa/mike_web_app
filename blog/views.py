8462dade5be961bd24eaec6ec24cd1375ec16166
michaeliyaomolere <mikoa04@gmail.com>
2019-08-09 16:48:14 -0400

    Creating pagination for single user posts

--- blog/views.py
+++ blog/views.py
@@ -30,11 +30,12 @@ class UserPostListView(ListView):
     model = Post
     template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    ordering = ['-date_posted']
     paginate_by = 5
 
-def get_query_set(self):
 
+def get_query_set(self):
+    user = get_object_or_404(User, username=self.kwargs.get('username'))
+    return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
