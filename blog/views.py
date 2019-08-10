4641fd354fde0f632dfed7b3fda7e408c31c9252
michaeliyaomolere <mikoa04@gmail.com>
2019-08-10 16:01:32 -0400

    Creating the URL pattern for user's views

--- blog/views.py
+++ blog/views.py
@@ -33,9 +33,9 @@ class UserPostListView(ListView):
     paginate_by = 5
 
 
-def get_query_set(self):
-    user = get_object_or_404(User, username=self.kwargs.get('username'))
-    return Post.objects.filter(author=user).order_by('-date_posted')
+    def get_query_set(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
